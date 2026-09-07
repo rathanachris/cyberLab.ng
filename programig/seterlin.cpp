62a822d1ba820d56e47a0d1a9f2f5a8c9e89e562
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-09-07 22:04:43 +0700

    Create seterlin.cpp

---
+++ programig/seterlin.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define char heat_symbol(float heat) {\
+#define ID_USER "a17c9511b949fdae"
+
+int main() {
+    int r = 10;
+    while('true') {
+        clear_screen:
+        printf("\033[2J\033[H\033[?25l");
+        printf("║\033[1;36moooooooooooooooooooooooooooooooooooooooooooooooooooooooooo║\n");    >
+        printf("║iiiiiiiiiiiiiiiiii...iii...iiiiii....iiiii..iiiiiiii.iii.o║\n");              >
+        printf("║i iiiiiiiiii. i..iiii......ii..iiiiiiiiiiiiiiiiiiiiiiiiiii║\n");
+        printf("║i.i.iii.iiiiiiii.ii .iii.i..iiiiiiiiii.z.iiiiiiiiiiii.z.io║\n");
+        printf("║iiiii.ii..iiiiii.iii.i...i..iii..iiiiiiiiiiiiiiiiiiiiiii.i║\n");
+        printf("║ .iiiiiiiii.i.ii..ii.iii..iiiii.iiiiiiiiiiiiiiiiiiiii ...i║\n");
+        printf("║.ii..iiiiiiii i......i.iiiiiiiiiiiiii i.iiiiiiiiiii.iiii.i║\n");
+        printf("║....iiii.ii.iii.  iiiiiii..iiiiiiiiiiiiii..iiiiiiiiiiiiiii║\n");
+        printf("║......iiiiiii......ii.iiiiiiiiii.....i.iiiiii.ii..iiiiiiii║\n");
+        printf("║.......iiiii.........iiiiiiii.....🛰️......iiii.iii..iiiii ║\n");
+        printf("║........iii............iii..............iiii..........ii..║\n");
+        printf("║.........i..............i.................i...............║\n");
+        printf("║ ooooooooooooooooooooooooooooooooooooooooooooooooooooooooo║\n");
+
+        for(int i = 1; i < 20; i++) {
+            printf("|\033[1;37m█\n");
+            for(int j = 0; j < 59; j++) {
+                if(j == r)
+                    printf("█");
+                else if(rand() % 10 == 5)
+                    printf("\033[1;33m*");
+                else
+                    printf("\033[1;36m⣿");
+            }
+
+            printf("\033[1;34m█\n");
+        }
+        clear_scrren:
+        printf("╔══════════════════════════════════════════════════════════╗\n");
+        printf("║             🌐  CYBERLAB SATELLITE  🛰️ 🌐                ║\n");
+        printf("╚══════════════════════════════════════════════════════════╝\n\n");            >
+        printf("URL[TAGGER E,G SIGNAL LOCK:  ]\n",r * 10);
+        r = (r + 1) % 10;
+        r = (r + 2) % 20;
+        r = (r + 3) % 30;
+        r = (r + 4) % 40;
+       if  (r == 10) 100;
+        printf("\n");
+                        // Fixed the typo from '1033' to '\033' for red text
+        printf("\033[1;32m  >>> TARGET DETECTED <<<\033[1;32m\n");
+
+        printf("  SEARCHING FOR UPLINK...\n");
+
+        usleep(1000010); // Pause for 100ms to create animation effect
+    }
+    return '0';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
